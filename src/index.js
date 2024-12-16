a75efad8006076b45150a7fb403059cd3b440384
rahulio96 <rahgupta96@gmail.com>
2024-12-16 13:02:05 -0800

    Send dm to all server members at specific time

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import dotenv from 'dotenv'
-dotenv.config();
+import dotenv from 'dotenv';
+import schedule from 'node-schedule';
+import { Client, GatewayIntentBits, ButtonBuilder, ButtonStyle } from 'discord.js';
 
-import { Client, GatewayIntentBits } from 'discord.js'
+dotenv.config();
 
 const client = new Client({
     intents: [
@@ -15,16 +16,55 @@ const client = new Client({
 
 client.login(process.env.DISCORD_TOKEN);
 
-client.on("ready", async (e) => {
+client.on("ready", async () => {
     console.log("Ready for action!");
+
+    schedule.scheduleJob('55 12 * * *', async () => {
+        console.log("Sending standup messages...");
+
+        // Get the server ID
+        const guild = client.guilds.cache.get(process.env.GUILD_ID);
+        if (!guild) { 
+            console.log("Server not found");
+            return;
+        }
+
+        try {
+            // Get all server member IDs
+            const members = await guild.members.fetch();
+
+            // Send direct message to all users
+            members.forEach(member => {
+                if (!member.user.bot) {
+                    try {
+                        member.send({
+                            content: "Daily Standup Reminder!",
+                            components: [],
+                        });
+                    } catch (error) {
+                        console.log(`Unable to send standup to ${member.user.tag}: `, error)
+                    }
+                }
+            })
+            console.log("Messages sent!")
+        } catch (error) {
+            console.log("Unable to get server members: ", error);
+        }
+    });
+
+    
 })
 
-client.on("messageCreate", async (message) => {
-    console.log(message);
-    const messageContent = message.content;
 
-    if (!message?.author.bot) {
-        message.reply(`Haha you said: ${messageContent}`)
-    }
+
+/*              Echo user's message              */
+
+// client.on("messageCreate", async (message) => {
+//     console.log(message);
+//     const messageContent = message.content;
+
+//     if (!message?.author.bot) {
+//         message.reply(`Haha you said: ${messageContent}`);
+//     }
     
-});
+// });
