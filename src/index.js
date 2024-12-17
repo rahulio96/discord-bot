427f319cf5dd3defd8e5368fdee38b7f0db68791
rahulio96 <rahgupta96@gmail.com>
2024-12-16 20:38:39 -0800

    Add intro prompt, inital reply, and buttons

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import dotenv from 'dotenv';
 import schedule from 'node-schedule';
-import { Client, GatewayIntentBits, ButtonBuilder, ButtonStyle } from 'discord.js';
+import { Client, GatewayIntentBits, ButtonBuilder, ButtonStyle, ActionRowBuilder } from 'discord.js';
 
 dotenv.config();
+
+const INTRO_PROMPT = "Hi, are your ready to fill out your **Daily Standup** update?";
+const HOUR = "9";
+const MINUTE = "0";
 
 const client = new Client({
     intents: [
@@ -16,10 +20,28 @@ const client = new Client({
 
 client.login(process.env.DISCORD_TOKEN);
 
+const yesBtn = new ButtonBuilder().setCustomId("yes").setLabel("Yes, I'm ready").setStyle(ButtonStyle.Primary);
+const snoozeBtn = new ButtonBuilder().setCustomId("snooze").setLabel("Snooze").setStyle(ButtonStyle.Secondary);
+const noBtn = new ButtonBuilder().setCustomId("no").setLabel("No, skip").setStyle(ButtonStyle.Secondary);
+
+const actionRow = new ActionRowBuilder().addComponents(yesBtn, snoozeBtn, noBtn);
+
+const replyToIntroPrompt = async ( interaction, text ) => {
+    await interaction.update({
+        content: `> ${text}`,
+        embeds: [
+            {
+                description: INTRO_PROMPT,
+            }
+        ],
+        components: [],
+    });
+}
+
 client.on("ready", async () => {
     console.log("Ready for action!");
 
-    schedule.scheduleJob('55 12 * * *', async () => {
+    // schedule.scheduleJob(`${MINUTE} ${HOUR} * * *`, async () => {
         console.log("Sending standup messages...");
 
         // Get the server ID
@@ -30,7 +52,7 @@ client.on("ready", async () => {
         }
 
         try {
-            // Get all server member IDs
+            // Get all server members
             const members = await guild.members.fetch();
 
             // Send direct message to all users
@@ -38,33 +60,40 @@ client.on("ready", async () => {
                 if (!member.user.bot) {
                     try {
                         member.send({
-                            content: "Daily Standup Reminder!",
-                            components: [],
+                            embeds: [{ description: INTRO_PROMPT }],
+                            components: [actionRow],
                         });
                     } catch (error) {
-                        console.log(`Unable to send standup to ${member.user.tag}: `, error)
+                        console.log(`Unable to send standup to ${member.user.tag}: `, error);
                     }
                 }
-            })
-            console.log("Messages sent!")
+            });
+            console.log("Messages sent!");
         } catch (error) {
             console.log("Unable to get server members: ", error);
         }
-    });
+    // });  
+});
 
-    
-})
+client.on("interactionCreate", async (interaction) => {
+    if (!interaction.isButton()) {
+        return;
+    }
 
+    const user = interaction.user;
 
+    // Switch case depending on button pressed
+    switch (interaction.customId) {
+        case "yes":
+            replyToIntroPrompt(interaction, "Yes, I'm ready");
+            break;
 
-/*              Echo user's message              */
+        case "snooze":
+            replyToIntroPrompt(interaction, "Snooze");
+            break;
 
-// client.on("messageCreate", async (message) => {
-//     console.log(message);
-//     const messageContent = message.content;
-
-//     if (!message?.author.bot) {
-//         message.reply(`Haha you said: ${messageContent}`);
-//     }
-    
-// });
+        case "no":
+            replyToIntroPrompt(interaction, "No, skip");
+            break;
+    }
+});
